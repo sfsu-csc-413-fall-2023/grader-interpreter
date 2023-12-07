7d1ad02665c88172a3c0cca947a7978d1aeaf8e1
John Roberts <roberts.john@gmail.com>
2023-12-06 20:19:35 -0800

    bop code test

--- tests/bytecode/BopCodeTest.java
+++ tests/bytecode/BopCodeTest.java
@@ -3,10 +3,18 @@ package tests.bytecode;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import interpreter.bytecode.BopCode;
+import interpreter.bytecode.ByteCode;
+import interpreter.bytecode.LitCode;
+import interpreter.loader.Program;
+import tests.helpers.TestVM;
 
 public class BopCodeTest {
   @Test
@@ -14,5 +22,57 @@ public class BopCodeTest {
     BopCode code = new BopCode(List.of("BOP", "+"));
 
     assertEquals("BOP +", code.toString());
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideOperators")
+  public void testOperator(List<ByteCode> instructions, int expectedValue) {
+    Program program = new Program();
+    instructions.stream().forEach(code -> program.addCode(code));
+
+    TestVM vm = new TestVM(program);
+    vm.executeToEnd(instructions.size());
+
+    assertEquals(
+        expectedValue,
+        vm.getRuntimeStack().peek());
+  }
+
+  private static LitCode lit(String val) {
+    return new LitCode(List.of("LIT", val));
+  }
+
+  private static BopCode bop(String op) {
+    return new BopCode(List.of("BOP", op));
+  }
+
+  private static Stream<Arguments> provideOperators() {
+    return Stream.of(
+        Arguments.of(List.of(lit("12"), lit("5"), bop("+")), 12 + 5),
+        Arguments.of(List.of(lit("13"), lit("5"), bop("-")), 13 - 5),
+        Arguments.of(List.of(lit("8"), lit("9"), bop("*")), 8 * 9),
+        Arguments.of(List.of(lit("8"), lit("3"), bop("/")), 8 / 3),
+        Arguments.of(List.of(lit("9"), lit("12"), bop("==")), 0),
+        Arguments.of(List.of(lit("7"), lit("7"), bop("==")), 1),
+        Arguments.of(List.of(lit("42"), lit("42"), bop("!=")), 0),
+        Arguments.of(List.of(lit("89"), lit("175"), bop("!=")), 1),
+        Arguments.of(List.of(lit("42"), lit("5"), bop(">")), 1),
+        Arguments.of(List.of(lit("5"), lit("42"), bop(">")), 0),
+        Arguments.of(List.of(lit("8"), lit("7"), bop(">=")), 1),
+        Arguments.of(List.of(lit("7"), lit("7"), bop(">=")), 1),
+        Arguments.of(List.of(lit("99"), lit("177"), bop(">=")), 0),
+        Arguments.of(List.of(lit("33"), lit("99"), bop("<")), 1),
+        Arguments.of(List.of(lit("34"), lit("2"), bop("<")), 0),
+        Arguments.of(List.of(lit("3"), lit("3"), bop("<=")), 1),
+        Arguments.of(List.of(lit("33"), lit("42"), bop("<=")), 1),
+        Arguments.of(List.of(lit("3"), lit("2"), bop("<=")), 0),
+        Arguments.of(List.of(lit("0"), lit("0"), bop("&")), 0),
+        Arguments.of(List.of(lit("0"), lit("1"), bop("&")), 0),
+        Arguments.of(List.of(lit("1"), lit("0"), bop("&")), 0),
+        Arguments.of(List.of(lit("1"), lit("1"), bop("&")), 1),
+        Arguments.of(List.of(lit("0"), lit("0"), bop("|")), 0),
+        Arguments.of(List.of(lit("0"), lit("1"), bop("|")), 1),
+        Arguments.of(List.of(lit("1"), lit("0"), bop("|")), 1),
+        Arguments.of(List.of(lit("1"), lit("1"), bop("|")), 1));
   }
 }
--- tests/helpers/TestVM.java
+++ tests/helpers/TestVM.java
@@ -7,6 +7,7 @@ import interpreter.VirtualMachine;
 import interpreter.loader.Program;
 
 public class TestVM extends VirtualMachine {
+  private int nextPc;
 
   public TestVM(Program program) {
     super(program);
@@ -14,11 +15,22 @@ public class TestVM extends VirtualMachine {
 
   public TestVM(Program program, Stack<Integer> returnAddresses, RuntimeStack runtimeStack) {
     super(program, returnAddresses, runtimeStack);
+    this.nextPc = 0;
   }
 
   @Override
   public void executeProgram() {
-    program.getCode(0).execute(this);
+    program.getCode(this.nextPc++).execute(this);
+  }
+
+  public void executeToEnd(int instructionCount) {
+    for (int i = 0; i < instructionCount; i++) {
+      program.getCode(i).execute(this);
+    }
+  }
+
+  public int getPc() {
+    return this.nextPc;
   }
 
   public Stack<Integer> getReturnAddresses() {
